a9bd7f24fe7f4d7a562de2d4f59cb0a6b0639cd1
Danish Rizwan <117601111+Dani78662@users.noreply.github.com>
2022-12-24 22:11:42 +0500

    Create code.cs

---
+++ Assignment 2/Question 5/code.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            Console.WriteLine("Enter number");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int a = 0;
+            int b = 1;
+            int c;
+            while (i < n)
+            {
+                c = a + b;
+                Console.WriteLine(c);
+                a = b;
+                b = c;
+                i++;
+            }
+            Console.ReadKey();
+
+           
+        }
+    }
+}
