8445d543401c2c7410af0daf4293cd8e600a2ce5
Danish Rizwan <117601111+Dani78662@users.noreply.github.com>
2022-12-31 22:41:07 +0500

    Create code.cs

---
+++ Assignment 1/Question 5/code.cs
@@ -0,0 +1,45 @@
+ Console.WriteLine("Enter floor 0-9");
+            int n=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter 7 digits");
+            int d=Convert.ToInt32(Console.ReadLine());
+            if (d == 1111110)
+            {
+                Console.WriteLine("The lift is on floor 0");
+            }
+            else if (d == 0110000)
+            {
+                Console.WriteLine("The lift is on floor 1");
+            }
+            else if (d == 1101101)
+            {
+                Console.WriteLine("The lift is on floor 2");
+            }
+            else if (d == 1111001)
+            {
+                Console.WriteLine("The lift is on floor 3");
+            }
+            else if (d == 0110011)
+            {
+                Console.WriteLine("The lift is on floor 4");
+            }
+            else if (d == 1011011)
+            {
+                Console.WriteLine("The lift is on floor 5");
+            }
+            else if (d == 1001111)
+            {
+                Console.WriteLine("The lift is on floor 6");
+            }
+            else if (d == 1110000)
+            {
+                Console.WriteLine("The lift is on floor 7");
+            }
+            else if (d == 1111111)
+            {
+                Console.WriteLine("The lift is on floor 8");
+            }
+            else if (d == 1110011)
+            {
+                Console.WriteLine("The lift is on floor 9");
+            }
+            Console.ReadKey();
