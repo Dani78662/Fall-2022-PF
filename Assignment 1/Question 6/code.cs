64dda51a0e00bdce01d6e5b0ba2cbc40a2a9b393
Danish Rizwan <117601111+Dani78662@users.noreply.github.com>
2023-01-02 13:30:29 +0500

    Update code.cs

--- Assignment 1/Question 6/code.cs
+++ Assignment 1/Question 6/code.cs
@@ -1,11 +1,12 @@
 Console.Write("Input  a number(integer): ");
             int n = Convert.ToInt32(Console.ReadLine());
-            int sum = 0;
-            while (n != 0)
+            int sum = 0, mod=0;
+            while (n > 0)
             {
-                sum += n % 10;
-                n /= 10;
+                mod = n % 10;
+                sum = mod + sum;
+               n = n / 10;
             }
             Console.WriteLine("Sum is: " + sum);
-        
-           Console.ReadKey();
+
+            Console.ReadKey();
